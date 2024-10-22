9e8022fac3b98be87dd1648a27a4d9cfb7783d44
Your Name <subasiarhan3@gmail.com>
2024-10-22 14:34:33 +0300

    updated strcut

--- CipherTorrent/zksnark/client.js
+++
@@ -1,8 +0,0 @@
-const snarkjs = require("snarkjs");
-
-async function generateProof(input, wasmFile, zkeyFile) {
-    const { proof, publicSignals } = await snarkjs.groth16.fullProve(input, wasmFile, zkeyFile);
-    return { proof, publicSignals };
-}
-
-module.exports = { generateProof };
